d4df3e11943d39e0260e59332cf07c7f634ab85c
Alex Bannow <rbannal@gmail.com>
2020-11-20 11:39:46 -0500

    minor fix tto the slideshow controls

--- src/Components/Slideshow/SlideshowControls.js
+++ src/Components/Slideshow/SlideshowControls.js
@@ -21,15 +21,17 @@ export default function SlideshowControls(props) {
     margin: "auto",
     height: "10vh",
     display: "flex",
-    flexDirection: "row",
+    flexDirection: "column",
     justifyContent: "space-evenly",
     position: "absolute",
     transition: "width 1s",
     bottom: "0",
     left: "0",
+    visibility: "hidden",
   };
 
-  if (showElement) styleObj = { ...styleObj, width: "100%" };
+  if (showElement)
+    styleObj = { ...styleObj, width: "100%", visibility: "visible" };
 
   const renderButtons = () => {
     console.log("renderbuttons");
@@ -50,21 +52,28 @@ export default function SlideshowControls(props) {
   };
   return (
     <div style={styleObj} className={"slide_show_controls_main "}>
-      <button
-        disabled={buttonPage === 0 ? true : false}
-        onClick={() => {
-          setCurrentButtons(null);
-          setButtonPage(buttonPage - 1);
-        }}
-      >{`<`}</button>
-      {currentButtons ? renderButtons() : null}
-      <button
-        disabled={(buttonPage + 1) * 10 > props.points.length}
-        onClick={() => {
-          setCurrentButtons(null);
-          setButtonPage(buttonPage + 1);
-        }}
-      >{`>`}</button>
+      <div className="slide_show_controls_play">
+        <button>Pause</button>
+        <button>Play</button>
+        <button>Exit</button>
+      </div>
+      <div className="slide_show_controls_points">
+        <button
+          disabled={buttonPage === 0 ? true : false}
+          onClick={() => {
+            setCurrentButtons(null);
+            setButtonPage(buttonPage - 1);
+          }}
+        >{`<`}</button>
+        {currentButtons ? renderButtons() : null}
+        <button
+          disabled={(buttonPage + 1) * 10 >= props.points.length}
+          onClick={() => {
+            setCurrentButtons(null);
+            setButtonPage(buttonPage + 1);
+          }}
+        >{`>`}</button>
+      </div>
     </div>
   );
 }
